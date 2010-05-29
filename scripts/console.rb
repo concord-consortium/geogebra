cb5d371f37c49a6623728bcefd531bf399d4cdd5
sergio <sergio@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-05-29 21:37:22 +0000

    Added scripts dir for console.rb script.
    git-svn-id: http://www.geogebra.org/svn/trunk@5789 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ scripts/console.rb
@@ -0,0 +1,45 @@
+# Run it with JRuby
+
+require 'rubygems'
+require 'irb'
+
+# Where the jar files are.
+JAR_DIR= File.dirname(__FILE__)
+
+%w{geogebra.jar
+   geogebra_main.jar
+   geogebra_properties.jar
+   geogebra_gui.jar
+}.each do |jar|
+  require File.join JAR_DIR, jar
+end
+
+# Just some constants.
+App = Java::GeogebraMain::Application
+DefaultApp = Java::GeogebraMain::DefaultApplication
+DropTarget = Java::JavaAwtDnd::DropTarget
+GgbFrame = Java::GeogebraGuiApp::GeoGebraFrame
+
+
+# Taken from GeoGebra codebase.
+@wnd = GgbFrame.new
+@app = DefaultApp.new([].to_java(:string), @wnd, true)
+
+@app.gui_manager.init_menubar
+
+# Init GUI
+@wnd.application=@app
+@wnd.content_pane.add @app.build_application_panel
+@wnd.drop_target = DropTarget.new(@wnd, Java::GeogebraGui::FileDropTargetListener.new(@app))
+@wnd.add_window_focus_listener @wnd
+@wnd.java_send :updateAllTitles
+
+#GeoSetConstructionOrder
+def @app.gsco
+  kernel.construction.geo_set_construction_order
+end
+
+puts 'GeoGebraFrame is stored in @wnd and Application in @app.'
+puts 'Remember gsco. If you want the GeoGebra window, just type:'
+puts '@wnd.visible = true'
+IRB.start
