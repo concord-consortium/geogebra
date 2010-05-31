b491a0b84cd652834932d6c38b79207d67f62aa5
gabor <gabor@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2010-05-31 15:35:37 +0000

    needed for Command
    git-svn-id: http://www.geogebra.org/svn/branches/ggjsviewer@5820 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

---
+++ src/org/geogebra/ggjsviewer/client/kernel/arithmetic3D/Vector3DValue.java
@@ -0,0 +1,37 @@
+/* 
+GeoGebra - Dynamic Mathematics for Everyone
+http://www.geogebra.org
+
+This file is part of GeoGebra.
+
+This program is free software; you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by 
+the Free Software Foundation.
+
+*/
+
+/*
+ * VectorValue.java
+ *
+ * Created on 03. Oktober 2001, 10:09
+ */
+
+package org.geogebra.ggjsviewer.client.kernel.arithmetic3D;
+
+import org.geogebra.ggjsviewer.client.kernel.arithmetic.ExpressionValue;
+
+
+
+
+
+/**
+ *
+ * @author  Markus + ggb3D
+ * @version 
+ */
+public interface Vector3DValue extends ExpressionValue { 
+    public double[] getPointAsDouble();    
+    //public int getMode(); // POLAR or CARTESIAN
+    //public void setMode(int mode);       
+	//AGpublic Geo3DVec get3DVec();
+}
